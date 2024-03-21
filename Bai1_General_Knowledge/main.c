038323897c383dc0f232b8e50c24fb9c9187db0c
Doan Duc Manh <manhdoanduc7@gmail.com>
2024-03-22 01:35:17 +0700

    Code Makefile

---
+++ Bai1_General_Knowledge/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "inc/hello_manh.h"
+#include "inc/hello_world.h"
+
+int main()
+{
+    hello_manh();
+    hello_world();
+
+    return 0;
+}
---
+++ Bai1_General_Knowledge/src/hello_manh.c
@@ -0,0 +1,6 @@
+#include "hello_manh.h"
+
+void hello_manh()
+{
+    printf("Hello I am Manh\n");
+}
